22ff0af2cb0f7b75a12a8f39a941d1a0499ae68d
Sergii Kostyrko <srg.kostyrko@gmail.com>
2024-12-08 10:16:08 +0200

    [green]: throws error when number > 100

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,4 +1,5 @@
 export function fizzbuzz(number: number): string {
+  if (number > 100) throw new RangeError("Number must be less than 100");
   if (number % 3 === 0 && number % 5 === 0) return "FizzBuzz";
   if (number % 3 === 0) return "Fizz";
   if (number % 5 === 0) return "Buzz";

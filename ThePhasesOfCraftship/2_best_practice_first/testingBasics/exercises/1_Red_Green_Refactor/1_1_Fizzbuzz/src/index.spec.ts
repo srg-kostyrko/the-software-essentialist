95249afa0cd1d8c5d8e3b20b45c126c6932bf6cf
Sergii Kostyrko <srg.kostyrko@gmail.com>
2024-12-08 10:00:29 +0200

    [red]: returns "Fizz" for multiples of 3

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,7 +1,10 @@
 import { fizzbuzz } from "./fizzbuzz";
 
 describe("fizzbuzz", () => {
-  it('should return "fizz" when passed 3', () => {
+  it('should return "Fizz" when passed 3', () => {
     expect(fizzbuzz(3)).toBe("Fizz");
   });
+  it('should return "Fizz" when passed multiples of 3', () => {
+    expect(fizzbuzz(6)).toBe("Fizz");
+  })
 });

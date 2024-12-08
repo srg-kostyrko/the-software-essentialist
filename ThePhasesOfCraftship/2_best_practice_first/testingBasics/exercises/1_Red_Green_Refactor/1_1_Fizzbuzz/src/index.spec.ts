59c95c90e53043ba426ee341647fc0008095f1da
Sergii Kostyrko <srg.kostyrko@gmail.com>
2024-12-08 10:10:51 +0200

    [red]: return string representation of number

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,6 +1,10 @@
 import { fizzbuzz } from "./fizzbuzz";
 
 describe("fizzbuzz", () => {
+  it("should return string representation of number", () => {
+    expect(fizzbuzz(1)).toBe("1");
+  });
+
   it('should return "Fizz" when passed 3', () => {
     expect(fizzbuzz(3)).toBe("Fizz");
   });
@@ -20,7 +24,7 @@ describe("fizzbuzz", () => {
     expect(fizzbuzz(15)).toBe("FizzBuzz");
   });
 
-  it ('should return "FizzBuzz" when passed multiples of 3 and 5', () => {
+  it('should return "FizzBuzz" when passed multiples of 3 and 5', () => {
     expect(fizzbuzz(30)).toBe("FizzBuzz");
-  })
+  });
 });

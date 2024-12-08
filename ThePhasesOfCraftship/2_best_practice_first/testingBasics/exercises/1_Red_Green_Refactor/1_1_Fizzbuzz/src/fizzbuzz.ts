91a9e9fc6bfdc4b022d22fe2bd65bae79adf85d6
Sergii Kostyrko <srg.kostyrko@gmail.com>
2024-12-08 10:04:52 +0200

    [green]: retursn "Buzz" when passed multiples of 5

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,5 +1,5 @@
 export function fizzbuzz(number: number): string {
   if (number % 3 === 0) return "Fizz";
-  if (number === 5) return "Buzz";
+  if (number % 5 === 0) return "Buzz";
   return "";
 }

63431eb1e4de10b74f9de911e81aebb1ae54ae87
Sergii Kostyrko <srg.kostyrko@gmail.com>
2024-12-08 10:27:00 +0200

    [refactor]: use it.each for sample testing

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,42 +1,32 @@
 import { fizzbuzz } from "./fizzbuzz";
 
 describe("fizzbuzz", () => {
-  it("should return string representation of number", () => {
-    expect(fizzbuzz(1)).toBe("1");
+  it.each([
+    [1, "1"],
+    [3, "Fizz"],
+    [4, "4"],
+    [5, "Buzz"],
+    [6, "Fizz"],
+    [9, "Fizz"],
+    [10, "Buzz"],
+    [11, "11"],
+    [12, "Fizz"],
+    [13, "13"],
+    [15, "FizzBuzz"],
+    [30, "FizzBuzz"],
+  ])("for %s it should return `%s`", (input, expected) => {
+    expect(fizzbuzz(input)).toBe(expected);
   });
 
-  it('should return "Fizz" when passed 3', () => {
-    expect(fizzbuzz(3)).toBe("Fizz");
-  });
-  it('should return "Fizz" when passed multiples of 3', () => {
-    expect(fizzbuzz(6)).toBe("Fizz");
-  });
-
-  it('should return "Buzz" when passed 5', () => {
-    expect(fizzbuzz(5)).toBe("Buzz");
-  });
-
-  it('should return "Buzz" when passed multiples of 5', () => {
-    expect(fizzbuzz(10)).toBe("Buzz");
-  });
-
-  it('should return "FizzBuzz" when passed 15', () => {
-    expect(fizzbuzz(15)).toBe("FizzBuzz");
-  });
-
-  it('should return "FizzBuzz" when passed multiples of 3 and 5', () => {
-    expect(fizzbuzz(30)).toBe("FizzBuzz");
-  });
-
-  it('should throw an error for numbers > 100', () => {
+  it("should throw an error for numbers > 100", () => {
     expect(() => fizzbuzz(101)).toThrowError(RangeError);
   });
 
-  it('should throw an error for numbers < 1', () => {
+  it("should throw an error for numbers < 1", () => {
     expect(() => fizzbuzz(-1)).toThrowError(RangeError);
   });
 
-  it('should throw an error for non-numbers', () => {
+  it("should throw an error for non-numbers", () => {
     expect(() => fizzbuzz("a" as any)).toThrowError(TypeError);
-  })
+  });
 });

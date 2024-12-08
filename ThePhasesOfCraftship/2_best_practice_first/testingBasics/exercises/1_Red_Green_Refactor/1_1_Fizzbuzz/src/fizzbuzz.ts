fc163c8c5c65e255e1922f6e0c274520bdb0d7b5
Sergii Kostyrko <srg.kostyrko@gmail.com>
2024-12-08 10:11:36 +0200

    [green]: returns string representation of number

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -2,5 +2,5 @@ export function fizzbuzz(number: number): string {
   if (number % 3 === 0 && number % 5 === 0) return "FizzBuzz";
   if (number % 3 === 0) return "Fizz";
   if (number % 5 === 0) return "Buzz";
-  return "";
+  return String(number);
 }

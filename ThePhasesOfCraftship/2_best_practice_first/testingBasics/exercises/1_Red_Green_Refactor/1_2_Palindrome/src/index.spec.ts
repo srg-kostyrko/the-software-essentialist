1ec72452d9d94ab137e78f70203d2eef661d24b4
Sergii Kostyrko <srg.kostyrko@gmail.com>
2024-12-14 09:08:37 +0200

    [red]: check Mom is palindrome

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -4,4 +4,8 @@ describe("palindrome checker", () => {
   it("knows that 'mom' is a palindrome", () => {
     expect(isPalindrome("mom")).toBe(true);
   });
+
+  it("knows that 'Mom' is a palindrome", () => {
+    expect(isPalindrome("Mom")).toBe(true);
+  });
 });

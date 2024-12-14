246f6200e830ac1836c9d4da81d45eafe31c90fa
Sergii Kostyrko <srg.kostyrko@gmail.com>
2024-12-14 09:11:34 +0200

    [red]: check Was It A Rat I Saw is a palindrome

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -8,4 +8,8 @@ describe("palindrome checker", () => {
   it("knows that 'Mom' is a palindrome", () => {
     expect(isPalindrome("Mom")).toBe(true);
   });
+
+  it("knows that 'Was It A Rat I Saw' is a palindrome", () => {
+    expect(isPalindrome("Was It A Rat I Saw")).toBe(true);
+  });
 });

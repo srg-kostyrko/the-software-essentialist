285e48f7fb16e9cc2a6950961675c4f353de6b56
Sergii Kostyrko <srg.kostyrko@gmail.com>
2024-12-14 09:19:02 +0200

    [refactor]: add more examples to tests

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -4,7 +4,13 @@ describe("palindrome checker", () => {
   test.each([
     ["mom", "is", true],
     ["Mom", "is", true],
+    ["MoM", "is", true],
+    ["Momx", "isn't", false],
+    ["xMomx", "is", true],
     ["Was It A Rat I Saw", "is", true],
+    ["Never Odd or Even", "is", true],
+    ["Never Odd or Even1", "isn't", false],
+    ["1Never Odd or Even1", "is", true],
   ])("knows that '%s' %s a palindrome", (input, _, expected) => {
     expect(isPalindrome(input)).toBe(expected);
   });

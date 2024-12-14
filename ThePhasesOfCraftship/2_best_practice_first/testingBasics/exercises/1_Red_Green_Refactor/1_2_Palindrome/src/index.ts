4fdbb9a8486e79ccf0438c9e32e4a0ca6c301222
Sergii Kostyrko <srg.kostyrko@gmail.com>
2024-12-14 09:22:14 +0200

    [refactor]: remove duplication

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -1,11 +1,12 @@
 export function isPalindrome(input: string): boolean {
-  return (
-    input.toLocaleLowerCase().replace(/[^a-z0-9]/g, "") ===
-    input
-      .toLocaleLowerCase()
-      .replace(/[^a-z0-9]/g, "")
-      .split("")
-      .reverse()
-      .join("")
-  );
+  const prepared = removeNonAlphaNumeric(input.toLocaleLowerCase());
+  return prepared === reverseString(prepared);
+}
+
+function reverseString(input: string): string {
+  return input.split("").reverse().join("");
+}
+
+function removeNonAlphaNumeric(input: string): string {
+  return input.replace(/[^a-z0-9]/g, "");
 }

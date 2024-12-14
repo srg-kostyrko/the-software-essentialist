d314d684faa467cb9f7edcee3a4996c4c7a5a574
Sergii Kostyrko <srg.kostyrko@gmail.com>
2024-12-14 09:27:10 +0200

    [refactor]: add describe to tests to document intent

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,17 +1,33 @@
 import { isPalindrome } from "./index";
 
 describe("palindrome checker", () => {
-  test.each([
-    ["mom", "is", true],
-    ["Mom", "is", true],
-    ["MoM", "is", true],
-    ["Momx", "isn't", false],
-    ["xMomx", "is", true],
-    ["Was It A Rat I Saw", "is", true],
-    ["Never Odd or Even", "is", true],
-    ["Never Odd or Even1", "isn't", false],
-    ["1Never Odd or Even1", "is", true],
-  ])("knows that '%s' %s a palindrome", (input, _, expected) => {
-    expect(isPalindrome(input)).toBe(expected);
+  describe("palindrome is a word reversed", () => {
+    test.each([
+      ["mom", "is"],
+      ["wow", "is"],
+      ["bill", "isn't"],
+    ])("knows that '%s' %s a palindrome", (input, expected) => {
+      expect(isPalindrome(input)).toBe(expected === "is");
+    });
+  });
+  describe("case does not matter", () => {
+    test.each([
+      ["mom", "is"],
+      ["Mom", "is"],
+      ["MoM", "is"],
+      ["Momx", "isn't"],
+    ])("knows that '%s' %s a palindrome", (input, expected) => {
+      expect(isPalindrome(input)).toBe(expected === "is");
+    });
+  });
+  describe("only alphanumeric characters matter", () => {
+    test.each([
+      ["Was It A Rat I Saw", "is", true],
+      ["Never Odd or Even", "is", true],
+      ["Never Odd or Even1", "isn't", false],
+      ["1Never Odd or Even1", "is", true],
+    ])("knows that '%s' %s a palindrome", (input, expected) => {
+      expect(isPalindrome(input)).toBe(expected === "is");
+    });
   });
 });

afa5fa6db4f08669e3674cefe4944ff93abd779c
Sergii Kostyrko <srg.kostyrko@gmail.com>
2024-12-21 09:19:13 +0200

    [docs]: document responsibilities, examples and API

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
@@ -1,3 +1,28 @@
+/**
+ * Function to validate a password.
+ * Criterias:
+ * Between 5 and 15 characters long
+ * - "aD2" - not valid
+ * - "aD2345" - valid
+ * - "aD23456789123456" - not valid
+ * Contains at least one digit
+ * - "aDabcd" - not valid
+ * - "aD1234" - valid
+ * Contains at least one upper case letter
+ * - "ad1234" - not valid
+ * - "AD1234" - valid
+ * Return an object containing a boolean result and an errors key that — 
+ * when provided with an invalid password — contains an error message or type for all errors in occurrence. 
+ * There can be multiple errors at a single time.
+ * 
+ * type ValidationResult = {
+ *    valid: true;
+ * } | {
+ *    valid: false;
+ *    errors: Array<string>;
+ * }
+ * function validatePassword(password: string): ValidationResult {}
+ */
 
 describe('password validator', () => {
 

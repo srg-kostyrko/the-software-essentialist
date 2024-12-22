f0a79c686b0f8064c4900fe439e5991b5af30093
Sergii Kostyrko <srg.kostyrko@gmail.com>
2024-12-22 10:24:01 +0200

    [refactor]: parametrize tests

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
@@ -11,20 +11,13 @@ describe("stats calculator", () => {
   });
 
   describe("min calculation", () => {
-    it("knows that min of empty sequence is 0", () => {
-      expect(calcualteSequenceStats([]).min).toBe(0);
-    });
-
-    it("knwos that min of positive number sequence is lowest number", () => {
-      expect(calcualteSequenceStats([2, 1, 5]).min).toBe(1);
-    });
-
-    it("knows that min of positive number sequence with 0 is 0", () => {
-      expect(calcualteSequenceStats([2, 1, 0, 5]).min).toBe(0);
-    });
-
-    it("knows that min of negative number sequence is lowest number", () => {
-      expect(calcualteSequenceStats([-2, -1, -5]).min).toBe(-5);
+    it.each([
+      [[], 0],
+      [[2, 1, 5], 1],
+      [[2, 1, 0, 5], 0],
+      [[-2, -1, -5], -5],
+    ])("knows that min of %s is %s", (input: number[], expected: number) => {
+      expect(calcualteSequenceStats(input).min).toBe(expected);
     });
   });
 });

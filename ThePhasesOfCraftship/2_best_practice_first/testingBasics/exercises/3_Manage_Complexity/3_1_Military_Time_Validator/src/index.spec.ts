e0eb0573979d498a70324c2e9cae3689446f8384
Sergii Kostyrko <srg.kostyrko@gmail.com>
2024-12-24 14:08:21 +0200

    [new]: add checks for valid inputs

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -1,18 +1,25 @@
 import { validateMilitaryTime } from "./index";
 
 describe("military time validator", () => {
+  it.each([`01:12 - 14:32`, `22:00 - 23:12`, "23:59 - 00:00"])(
+    "knows that `%s` is valid",
+    (time) => {
+      expect(validateMilitaryTime(time)).toBeTruthy();
+    }
+  );
+
   it.each`
-    time             | reason
-    ${""}            | ${"empty input"}
-    ${"01:12 14:32"} | ${"missing separator between start and end times"}
-    ${"- 14:32"}     | ${"missing start time"}
-    ${"01:12 -"}     | ${"missing end time"}
-    ${"0112 - 14:32"} | ${"missing separator in start time"}
-    ${"01:12 - 1432"} | ${"missing separator in end time"}  
-    ${"1:12 - 14:32"} | ${"missing leading zero in start hour"}
-    ${"01:12 - 4:32"} | ${"missing leading zero in end hour"}
-    ${"01:2 - 14:32"} | ${"missing leading zero in start minutes"}
-    ${"01:12 - 14:3"} | ${"missing leading zero in end minutes"}
+    time               | reason
+    ${""}              | ${"empty input"}
+    ${"01:12 14:32"}   | ${"missing separator between start and end times"}
+    ${"- 14:32"}       | ${"missing start time"}
+    ${"01:12 -"}       | ${"missing end time"}
+    ${"0112 - 14:32"}  | ${"missing separator in start time"}
+    ${"01:12 - 1432"}  | ${"missing separator in end time"}
+    ${"1:12 - 14:32"}  | ${"missing leading zero in start hour"}
+    ${"01:12 - 4:32"}  | ${"missing leading zero in end hour"}
+    ${"01:2 - 14:32"}  | ${"missing leading zero in start minutes"}
+    ${"01:12 - 14:3"}  | ${"missing leading zero in end minutes"}
     ${"25:12 - 14:32"} | ${"start hour is out of range"}
     ${"01:12 - 25:32"} | ${"end hour is out of range"}
     ${"01:60 - 14:23"} | ${"start minutes is out of range"}

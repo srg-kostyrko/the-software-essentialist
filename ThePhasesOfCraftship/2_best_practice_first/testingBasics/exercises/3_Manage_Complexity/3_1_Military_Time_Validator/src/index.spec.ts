1cf213272b328da1f8a3da2c63f4abd30f88f0dd
Sergii Kostyrko <srg.kostyrko@gmail.com>
2024-12-24 12:16:23 +0200

    [refactor]: parametrize tests

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -1,15 +1,12 @@
 import { validateMilitaryTime } from "./index";
 
 describe("military time validator", () => {
-  it("knows that empty input is invalid", () => {
-    expect(validateMilitaryTime("")).toBe(false);
+  it.each`
+    time             | reason
+    ${""}            | ${"empty input"}
+    ${"01:12 14:32"} | ${"missing separator between start and end times"}
+    ${"- 14:32"}     | ${"missing start time"}
+  `("knows that `$time` is invalid because of $reason", ({ time }) => {
+    expect(validateMilitaryTime(time)).toBeFalsy();
   });
-
-  it("knows that '01:12 14:32' is invalid because of missing separator", () => {
-    expect(validateMilitaryTime("01:12 14:32")).toBe(false);
-  });
-
-  it("knows that `- 14:32` is invalid because of missing start time", () => {
-    expect(validateMilitaryTime("- 14:32")).toBe(false);
-  })
 });

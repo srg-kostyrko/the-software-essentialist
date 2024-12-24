e0f23a4bd59eef261702f6a35970b0d42098b68d
Sergii Kostyrko <srg.kostyrko@gmail.com>
2024-12-24 13:46:10 +0200

    [new]: validate presence of separtor in start time

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -7,6 +7,7 @@ describe("military time validator", () => {
     ${"01:12 14:32"} | ${"missing separator between start and end times"}
     ${"- 14:32"}     | ${"missing start time"}
     ${"01:12 -"}     | ${"missing end time"}
+    ${"0112 - 14:32"} | ${"missing separator in start time"}
   `("knows that `$time` is invalid because of $reason", ({ time }) => {
     expect(validateMilitaryTime(time)).toBeFalsy();
   });
--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -3,6 +3,7 @@ export function validateMilitaryTime(time: string): boolean {
   if (!time.includes("-")) return false;
   let [from, to] = time.split("-");
   if (!from) return false;
+  if (!from.includes(":")) return false;
   if (!to) return false;
   return true;
 }

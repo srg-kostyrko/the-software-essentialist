63aeb69c5a23c359006a88dc3f06735f08e343ab
Sergii Kostyrko <srg.kostyrko@gmail.com>
2024-12-25 16:44:13 +0200

    [refactor]: use stack based calculations

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
@@ -1,17 +1,40 @@
 function resolveLiteral(input: string): boolean {
   return input === "TRUE";
 }
+function valueToLiteral(input: boolean): string {
+  return input ? "TRUE" : "FALSE";
+}
+function isOperator(input: string): boolean {
+  return input === "NOT" || input === "AND" || input === "OR";
+}
+function isLiteral(input: string): boolean {
+  return input === "TRUE" || input === "FALSE";
+}
 
 export function resolveBooleanExpression(input: string): boolean {
   const tokens = input.split(" ");
-  if (tokens.length === 3 && tokens[1] === "AND") {
-    return resolveLiteral(tokens[0]) && resolveLiteral(tokens[2]);
+  const stack: string[] = [];
+  for (const token of tokens) {
+    if (isOperator(token)) {
+      stack.push(token);
+    } else if (isLiteral(token)) {
+      if (stack.length > 0 && isOperator(stack.at(-1)!)) {
+        const op = stack.pop();
+        if (op === "NOT") {
+          stack.push(valueToLiteral(!resolveLiteral(token)));
+        } else {
+          const left = stack.pop();
+          const right = token;
+          stack.push(
+            op === "AND"
+              ? valueToLiteral(resolveLiteral(left!) && resolveLiteral(right))
+              : valueToLiteral(resolveLiteral(left!) || resolveLiteral(right))
+          );
+        }
+      } else {
+        stack.push(token);
+      }
+    }
   }
-  if (tokens.length === 3 && tokens[1] === "OR") {
-    return resolveLiteral(tokens[0]) || resolveLiteral(tokens[2]);
-  }
-  if (tokens[0] === "NOT") {
-    return !resolveLiteral(tokens[1]);
-  }
-  return resolveLiteral(tokens[0]);
+  return resolveLiteral(stack[0]);
 }

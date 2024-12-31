c2064834449c6bc96d9d7a62ceed771e7dbed0ed
Sergii Kostyrko <srg.kostyrko@gmail.com>
2024-12-31 09:22:32 +0200

    refactor: improve error handling

--- ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart1/exercises/1_RefactoringTo4Tiers/begin/src/infra/error-handler.ts
+++ ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart1/exercises/1_RefactoringTo4Tiers/begin/src/infra/error-handler.ts
@@ -1,13 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { errorResponse } from "../shared/response";
 import { ErrorExceptionType } from "../shared/constants";
-import {
-  DuplicatedClassEnrollmentException,
-  InvalidIdException,
-  InvalidRequestBodyException,
-  InvalidRequestParamsException,
-  StudentNotFoundException,
-} from "../shared/exceptions";
+import { AppException } from "../shared/exceptions";
 
 export type ErrorHandler = (
   error: Error,
@@ -17,23 +11,9 @@ export type ErrorHandler = (
 ) => Response;
 
 export const errorHandler: ErrorHandler = (error, req, res, next) => {
-  let code = 500;
-  let errorType = ErrorExceptionType.ServerError;
-
-  if (
-    error instanceof InvalidRequestParamsException ||
-    error instanceof InvalidRequestBodyException ||
-    error instanceof InvalidIdException
-  ) {
-    code = 400;
-    errorType = ErrorExceptionType.ValidationError;
-  } else if (error instanceof StudentNotFoundException) {
-    code = 404;
-    errorType = ErrorExceptionType.StudentNotFound;
-  } else if (error instanceof DuplicatedClassEnrollmentException) {
-    code = 409;
-    errorType = ErrorExceptionType.StudentAlreadyEnrolled;
+  if (error instanceof AppException) {
+    return errorResponse(res, error.type, error.message, error.statusCode);
   }
 
-  return errorResponse(res, errorType, error.message, code);
+  return errorResponse(res, ErrorExceptionType.ServerError, error.message, 500);
 };
--- ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart1/exercises/1_RefactoringTo4Tiers/begin/src/shared/exceptions.ts
+++ ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart1/exercises/1_RefactoringTo4Tiers/begin/src/shared/exceptions.ts
@@ -1,45 +1,69 @@
-export class InvalidRequestParamsException extends Error {
+import { ErrorExceptionType } from "./constants";
+
+export class AppException extends Error {
+  constructor(
+    public readonly type: ErrorExceptionType,
+    public readonly statusCode: number,
+    message: string
+  ) {
+    super(message);
+  }
+}
+
+export class InvalidRequestParamsException extends AppException {
   constructor(missingKeys: string[]) {
     super(
+      ErrorExceptionType.ValidationError,
+      400,
       "Request params are missing the following keys: " + missingKeys.join(", ")
     );
   }
 }
 
-export class InvalidRequestBodyException extends Error {
+export class InvalidRequestBodyException extends AppException {
   constructor(missingKeys: string[]) {
     super(
+      ErrorExceptionType.ValidationError,
+      400,
       "Request body is missing the following keys: " + missingKeys.join(", ")
     );
   }
 }
 
-export class InvalidIdException extends Error {
+export class InvalidIdException extends AppException {
   constructor(id: string) {
-    super("Invalid id: " + id);
+    super(ErrorExceptionType.ValidationError, 400, "Invalid id: " + id);
   }
 }
 
-export class StudentNotFoundException extends Error {
+export class StudentNotFoundException extends AppException {
   constructor(id: string) {
-    super("Student not found: " + id);
+    super(ErrorExceptionType.StudentNotFound, 404, "Student not found: " + id);
   }
 }
 
-export class ClassNotFoundException extends Error {
+export class ClassNotFoundException extends AppException {
   constructor(id: string) {
-    super("Class not found: " + id);
+    super(ErrorExceptionType.ClassNotFound, 404, "Class not found: " + id);
   }
 }
 
-export class AssignmentNotFoundException extends Error {
+export class AssignmentNotFoundException extends AppException {
   constructor(id: string) {
-    super("Assignment not found: " + id);
+    super(
+      ErrorExceptionType.AssignmentNotFound,
+      404,
+      "Assignment not found: " + id
+    );
   }
 }
 
-export class DuplicatedClassEnrollmentException extends Error {
+export class DuplicatedClassEnrollmentException extends AppException {
   constructor(classId: string, studentId: string) {
-    super("Class " + classId + " already has student " + studentId);
+    super(
+      ErrorExceptionType.StudentAlreadyEnrolled,
+      409,
+      "Class " + classId + " already has student " + studentId
+    );
   }
 }
